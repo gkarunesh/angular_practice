7f3f145a99e1d780bd9c774e5c974ec4db94ce49
gauravkarunesh88@gmail.com <gauravkarunesh88@gmail.com>
2024-10-22 14:57:29 +0530

    angular 18 tutorial practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule, MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_practice';
+  title = 'angular18_practice';
+  city = signal("Patna");
+
+  changeCity(){
+    this.city.set("Delhi");
+  }
 }
